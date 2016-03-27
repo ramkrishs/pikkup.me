65ddbb9b26c47464ce8522a0b67e360e58789b7c
Ramakrishnan Sathyavageeswaran <ramkrishs@outlook.com>
2016-03-26 19:12:04 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	
+	$('.s-icons').hover(function() {
+		var icon  = $(this).find('.fa');
+		icon.css('color', '#54B49F');
+	}, function() {
+		var icon  = $(this).find('.fa');
+		icon.css('color', '#BDCBCB');
+	});
+});
